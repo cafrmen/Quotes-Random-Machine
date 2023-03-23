68b0b1b05e65a0bc62ec71056be7e4f7b4086f9d
cafrmen <cafrmen@gmail.com>
2023-03-22 20:16:09 -0600

    Random Quotes
    Portfolio project using React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const[quote, setQuote] = useState('');
+
+  //API from freeCodeCamp
+  const fetchData = () => {
+      fetch(`https://type.fit/api/quotes`)
+          .then((response) => response.json())
+          .then((data) => {
+              let indexNum = Math.floor(Math.random() * 1644);
+              setQuote(data[indexNum]);
+          });
+  }
+
+  useEffect (() => {
+      fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <div className="quotes" >
+          <blockquote>
+              <p className="text">"{quote.text}"</p>
+              <p className="author">{quote.author}</p>
+          </blockquote>
+          <div className="buttons">
+            <button onClick={fetchData} >New Quote</button>
+            <button className="copy" onClick={() =>  {navigator.clipboard.writeText(`${quote.text}-${quote.author}`)}} >Copy Quote</button>
+          </div>
+          </div>
+      </div>
+  )
 }
 
 export default App;
